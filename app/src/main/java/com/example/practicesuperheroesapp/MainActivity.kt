197aa1e8a26aa81876f289df6db8c3112639129f
Sukrit Rerk-u-cote <sukrit_love@Sukrits-MacBook-Pro.local>
2023-11-13 09:43:59 +0700

    Add Function to called data and Create UI

--- app/src/main/java/com/example/practicesuperheroesapp/MainActivity.kt
+++ app/src/main/java/com/example/practicesuperheroesapp/MainActivity.kt
@@ -3,25 +3,41 @@ package com.example.practicesuperheroesapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring.DampingRatioLowBouncy
+import androidx.compose.animation.core.Spring.StiffnessVeryLow
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.slideInVertically
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
@@ -29,6 +45,9 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.practicesuperheroesapp.DataSource.Hero
+import com.example.practicesuperheroesapp.DataSource.HeroesRepository
+import com.example.practicesuperheroesapp.DataSource.HeroesRepository.heroes
 import com.example.practicesuperheroesapp.ui.theme.PracticeSuperheroesAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -41,44 +60,25 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    SuperHeroList()
+                    CreateSuperHeroList()
                 }
             }
         }
     }
 }
 
-@Composable
-fun SuperHeroList(modifier: Modifier = Modifier) {
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        HeroCard()
-    }
-}
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HeroCard() {
-    Card {
-        Box(
-            modifier = Modifier
-                .padding(dimensionResource(R.dimen.padding))
-                .height(dimensionResource(R.dimen.image_size))
-        ) {
-            Row() {
-
-                Column {
-                    Text(
-                        text = stringResource(id = R.string.hero1),
-                        style = MaterialTheme.typography.displaySmall,
-                    )
-                    Text(
-                        text = stringResource(id = R.string.description1),
-                        style = MaterialTheme.typography.bodyLarge
-                    )
-                }
-                Image(
-                    painter = painterResource(id = R.drawable.android_superhero1),
-                    contentDescription = null,
-                    modifier = Modifier.padding(dimensionResource(R.dimen.padding))
+fun CreateSuperHeroList(modifier: Modifier = Modifier) {
+    Scaffold(
+        topBar = {HeroTopBar()}
+    ) { it ->
+        LazyColumn(contentPadding = it) {
+            itemsIndexed(heroes) { index, hero ->
+                HeroCard(
+                    heroes = hero,
+                    modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
                 )
             }
         }
@@ -86,10 +86,59 @@ fun HeroCard() {
 
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HeroTopBar(){
+    CenterAlignedTopAppBar(title = { Text(stringResource(id = R.string.app_name), style = MaterialTheme.typography.displayLarge) })
+}
+
+
+@Composable
+fun HeroCard(heroes: Hero, modifier: Modifier = Modifier) {
+    Card(
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp), modifier = Modifier
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+    ) {
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .sizeIn(minHeight = 72.dp)
+        ) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = stringResource(heroes.nameRes),
+                    style = MaterialTheme.typography.displaySmall,
+                )
+                Text(
+                    text = stringResource(heroes.descriptionRes),
+                    style = MaterialTheme.typography.bodyLarge
+                )
+            }
+            Spacer(Modifier.width(dimensionResource(R.dimen.padding)))
+            Box(
+                modifier = Modifier
+                    .size(72.dp)
+                    .clip(RoundedCornerShape(8.dp))
+
+            ) {
+                Image(
+                    painter = painterResource(heroes.imageRes),
+                    contentDescription = null,
+                    alignment = Alignment.TopCenter,
+                    contentScale = ContentScale.FillWidth
+                )
+            }
+        }
+    }
+}
+
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     PracticeSuperheroesAppTheme {
-        HeroCard()
+        CreateSuperHeroList()
     }
 }
